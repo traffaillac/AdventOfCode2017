2ef01247791a711c45630430c006c2a745ea5125
Thibault Raffaillac <traf@kth.se>
2017-12-27 16:10:22 +0100

    Fixed Day 16

--- day16.c
+++ day16.c
@@ -1,40 +1,48 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-static char str[48405];
+static char perms[32][16] = {"abcdefghijklmnop"};
+
+static void dance(char *perm) {
+	char old[16];
+	int c, i, j;
+	rewind(stdin);
+	do {
+		c = getchar();
+		memcpy(old, perm, sizeof(old));
+		if (c == 's') {
+			scanf("%d", &i);
+			memcpy(perm, old + sizeof(old) - i, i);
+			memcpy(perm + i, old, sizeof(old) - i);
+		} else {
+			if (c == 'x') {
+				scanf("%d/%d", &i, &j);
+			} else {
+				assert(c=='p');
+				int A = getchar();
+				for (i = 0; perm[i] != A; i++);
+				getchar();
+				int B = getchar();
+				for (j = 0; perm[j] != B; j++);
+			}
+			perm[i] = old[j];
+			perm[j] = old[i];
+		}
+	} while (getchar() == ',');
+}
 
 int main() {
-	static const char init[16] = "abcdefghijklmnop";
-	char list[16], old[16];
-	memcpy(list, init, 16);
-	gets(str);
-	do {
-		for (int i = 0; str[i] != 0; i++) {
-			//fputc(i, stderr);
-			memcpy(old, list, 16);
-			if (str[i] == 's') {
-				scanf("%d", &i);
-				memcpy(list, old + 16 - i, i);
-				memcpy(list + i, old, 16 - i);
-			} else if (i == 'x') {
-				scanf("%d/%d", &i, &j);
-				list[i] = old[j];
-				list[j] = old[i];
-			} else {
-				assert(i=='p');
-				scanf("%c/%c", &c, &d);
-				for (i = 0; list[i] != c - 'a'; i++);
-				for (j = 0; list[j] != d - 'a'; j++);
-				list[i] = old[j];
-				list[j] = old[i];
-			}
-		}
-	} while ();
-	v16qi shuf = perm;
-	for (int i = 0; i < 1; i++)
-		perm = __builtin_shuffle(perm, shuf);
+	for (int P = 1; P <= 30; P++) {
+		memcpy(perms[P], perms[P - 1], 16);
+		dance(perms[P]);
+		/*for (int p = 0; p < P; p++) {
+			if (memcmp(perms[p], perms[P], 16) == 0)
+				printf("perms[%d] == perms[%d]\n", p, P), exit(0);
+		}*/
+	}
 	for (int i = 0; i < 16; i++)
-		putchar('a' + perm[i]);
+		putchar(perms[10][i]);
 	putchar('\n');
 }
